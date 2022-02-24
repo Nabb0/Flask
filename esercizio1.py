79798857937f406e91b75d96e8e377ba899ed96d
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-02-24 10:51:52 +0000

    Finito l esercizio 2

--- esercizio1.py
+++ esercizio1.py
@@ -5,6 +5,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+import random
+
 @app.route('/')
 def hello_world():
     return render_template("Esercizio.html")
@@ -14,9 +16,17 @@ def hello_world():
 #la previsione è "pioggia", se è compreso tra 3 e 5 la previsione è "nuvoloso", se è maggiore di 5 la previsione è "sole".
 #Abbinare ad ogni previsione un'immagine adatta. Utilizzare un css per definire la grafica. La route per accedere al serizio deve essere /meteo
 
-@app.route('/meteo')
+@app.route("/meteo")
 def meteo():
-    return render_template("Esercizio.html")
+    nRandom = random.randint(0,8)
+    if nRandom <= 2:
+        immagine = "/static/images/Troppa-pioggia-a-livello-globale-aumentano-le-precipitazioni-estreme_articleimage.jpg"
+    elif nRandom <= 5:
+        immagine = "/static/images/meteo-nuvole-185033.660x368.jpg"
+    else:
+        immagine = "/static/images/Sole-Spiaggia-Mare.jpg"
+    return render_template("Meteo.html", meteo=immagine)
+
 #ultima riga
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
