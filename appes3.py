1c8dbe925da91bfa17ff6feb06ae3233f8b88c33
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-03-11 18:19:26 +0000

    ES3

--- appes3.py
+++ appes3.py
@@ -4,28 +4,52 @@
 #modificare poi in l es precedente per permettere al utente di inserire un capoluogo e di avere la regione in cui si trova
 #l utente sceglie se avere la regione o il capo luogo selezionado un radio button 
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template, request
 app = Flask(__name__)
 
+capoluoghiRegione = {'Abruzzo':'LAquila' , 'Basilicata':'Potenza' , 'Calabria':'Catanzaro' , 'Campania':'Napoli' ,
+  'EmiliaRomagna':'Bologna' , 'Friuli':'Trieste' , 'Lazio':'Roma' , 'Liguria':'Genova' , 'Lombardia':'Milano' ,
+  'Marche':'Ancona' , 'Molise':'Campobasso' , 'Piemonte':'Torino' , 'Puglia':'Bari' , 'Sardegna':'Cagliari' , 'Sicilia':'Palermo' , 
+  'Toscana':'Firenze' , 'Trentino':'Trento' , 'Umbria':'Perugia' , 'ValleDAosta':'Aosta' , 'Veneto':'Venezia'}
 
-capoluoghiRegione = {"Abruzzo": "Aquila", "Basilicata": "Potenza", "Calabria": "Catanzaro", "Campania": "Napoli", "Emilia-Romagna": "Bologna", "Friuli-venezia-Giulia": "Trieste", "Lazio": "Roma", "Liguaria": "Genova", "Lombardia": "Milano", "Marche": "Ancona", "Molise": "Campobasso", "Piemonte": "Torino", "Puglia": "Bari", "Sardegna": "Cagliari", "Sicilia": "Palermo", "Toscana": "Firenze", "Trentino-Alto Adige": "Trento", "Umbria": "Perugia", "valle d'Aosta": "Aosta", "Veneto": "Venezia"}
-
+Reg = list(capoluoghiRegione.keys())
+Cap = list(capoluoghiRegione.values())
 
 @app.route('/', methods=['GET'])
-def hello_world():
-   return render_template('RegioneOcapo.html')
+def RC():
+    return render_template('RegioniCapoluoghi.html')
 
-@app.route('/dates', methods=['GET'])
-def dates():
- Scelta = request.args['Scelta'] #request = i dati dal client
- if Scelta =='regione':
-     return render_template('Regioni.html')
- else:
-     return render_template('Kapoluoghi.html')
 
-    
+@app.route('/capreg', methods=['GET'])
+def CR():
+    CR = request.args['Tipo']
+    if CR == 'Capoluogo':
+        return render_template('Capoluogo.html')
+    else: 
+        return render_template('Regione.html')
 
- 
+
+@app.route("/reg", methods=["GET"])
+def dataReg():
+    regione = request.args["Regione"]
+    for key, value in capoluoghiRegione.items():
+        if regione == key:
+            capoluogo = value
+            return render_template("risultato.html", risposta = capoluogo)
+    return "<h1>Errore</h1>"
+
+@app.route("/cap", methods=["GET"])
+def dataCap():
+    capoluogo = request.args["Capoluogo"]
+    for key, value in capoluoghiRegione.items():
+        if capoluogo == value:
+             regione = key
+             return render_template("risultato.html", risposta = regione)
+    return "<h1>Errore</h1>"
+
+
+
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
