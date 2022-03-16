30c82e93a261f515139860c2cd78fdf7fcd14a34
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-03-16 13:56:35 +0000

    pippo

--- appes5.py
+++ appes5.py
@@ -5,51 +5,60 @@
 from flask import Flask, render_template,request
 app = Flask(__name__)
 import pandas as pd
-lista=[] #struttura dati temporanea va salvata sulla ram 
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('squadraHome.html')
+    return render_template('indexs.html')
+
+
+@app.route('/formricerca', methods=['GET'])
+def formricerca():
+    return render_template('formricerca.html')
+
+
+@app.route('/ricerca', methods=['GET'])
+def ricerca():
+    indice = request.args['indice']
+    radio = request.args['sel']
+    df1 = pd.read_csv('squads_database.csv')
+    if indice not in df1[radio].to_list():
+        return render_template('error.html')
+    
+    if radio == 'squadra':
+        return df1[df1['squadra'].str.contains(indice)].to_html()
+    if radio == 'anno':
+        return df1[df1['anno'].str.contains(indice)].to_html()
+    if radio == 'citta':
+        return df1[df1['citta'].str.contains(indice)].to_html()
+    
+    #return render_template('indexs2.html')
+
 
 @app.route('/inserisci', methods=['GET'])
 def inserisci():
-    return render_template('inserisci.html')
+    return render_template('indexs1.html')
 
 @app.route('/dati', methods=['GET'])
 def dati():
-    # inserimento dei dati nel file csv
-    # lettura dei dati dal form html 
+    
+    
     squadra = request.args['Squadra']
     anno = request.args['Anno']
     citta = request.args['Citta']
-    # lettura dei dati daal file nel dataframe
-    df1 = pd.read_csv('/workspace/Flask/templates/dati.csv')
-    # aggiungiamo i nuovi dati nel dataframe 
+    
+    
+    df1 = pd.read_csv('squads_database.csv')
+    
+    
     nuovi_dati = {'squadra':squadra,'anno':anno,'citta':citta}
     
     df1 = df1.append(nuovi_dati,ignore_index=True)
-    # salviamo il dataframe sul file dati.csv
-    df1.to_csv('/workspace/Flask/templates/dati.csv', index=False)
-    return df1.to_html()
-
-@app.route('/datiricerca', methods=['GET'])
-def ricerca():
-    return render_template('datiricerca.html')
-
-@app.route('/ricerca', methods=['GET'])
-def ric():
-    squadra = request.args['Squadra']
-    anno = request.args['Anno']
-    citta = request.args['Citta']
-    #caricamento dati dal file csv a un dataframe 
-    df2= pd.read_csv('/workspace/Flask/templates/dati.csv')
     
-
-
-    #ricerca dei dati 
-    df3=df2[df2['squadra']==squadra]
-    return df3.to_html()
-
+    
+    df1.to_csv('squads_database.csv', index=False)
+    rdf1 = df1.to_html()
+    #return df1.to_html()
+    return render_template('indexs2.html', tables=[rdf1], titles=[''])
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3246, debug=True)
