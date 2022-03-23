9c9ae60a91e0cbd4c9e35fa4180239c13b6e743a
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-03-23 22:05:36 +0000

    Finito

--- appes6.py
+++ appes6.py
@@ -2,8 +2,7 @@
 #ci deve essere una homepage con un link "quartieri di milano":
 #cliccando su questo link si deve visualizzare la mappa dei quartieri di milano 
 
-
-from flask import Flask, render_template, send_file, make_response, url_for, Response
+from flask import Flask, render_template, send_file, make_response, url_for, Response,request
 app = Flask(__name__)
 
 import io
@@ -15,39 +14,84 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-milano = gpd.read_file('/workspace/Flask/ds964_nil_wm.zip')
 
+quartieri = gpd.read_file('/workspace/Flask/ds964_nil_wm.zip')
+fontanelle = gpd.read_file('/workspace/Flask/Fontanelle.zip')
+# 1
 @app.route('/', methods=['GET'])
-def hello_world():
-    return render_template('simple.html')
+def home_page():
+    return render_template('home.html')
 
-@app.route('/cerca', methods=['GET'])
-def cerca():
-    return render_template('cerca.html')   
 
-@app.route('/cerca1', methods=['GET'])
+@app.route('/visualizza', methods=['GET'])
+def visualizza():
 
-def cerca():
-    Quartiere  = request.args['Quartiere']
+    fig, ax = plt.subplots(figsize = (12,8))
 
-    
+    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5,edgecolor='k')
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+
+    return Response(output.getvalue(), mimetype='image/png')
+#2
+@app.route('/quartiere', methods=['GET'])
+def home1_page():
+
+    return render_template('quartiere.html')
 
 @app.route('/quartieri.png', methods=['GET'])
 def plot_png():
 
     fig, ax = plt.subplots(figsize = (12,8))
 
-    milano.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+    imgUtente.to_crs(epsg=3857).plot(ax=ax, alpha=0.5,edgecolor='k')
     contextily.add_basemap(ax=ax)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-@app.route('/quartieri', methods=("POST", "GET"))
+@app.route('/risultato', methods=("POST", "GET"))
 def mpl():
-    return render_template('plot.html',
-                           PageTitle = "Matplotlib")
+    global imgUtente
 
+    qrt = request.args['quartiere']
+    imgUtente = quartieri[quartieri['NIL']==qrt]
+
+    return render_template('risultato.html',PageTitle = "Matplotlib",quartiere=qrt)
+#3
+
+@app.route('/scelta', methods=['GET'])
+def scelta():
+    qrt = quartieri.NIL.to_list()
+    qrt.sort()
+    return render_template('scelta.html',PageTitle = "Matplotlib",quartieri=qrt)
+#4 
+@app.route("/fontanelle", methods=["GET"])
+def fontanelle1():
+    return render_template("fontanelle.html", quartieri= quartieri["NIL"])
+
+@app.route('/fontanelleres', methods=("POST", "GET"))
+def fontanelleRes():
+    global imgUtente, fontQuart
+    
+    quartiereUtente = request.args["Quartiere"]
+    imgUtente = quartieri[quartieri["NIL"] == quartiereUtente]
+    fontQuart = fontanelle[fontanelle.within(imgUtente.geometry.squeeze())]
+    print(fontQuart)
+    return render_template('fontanelleRes.html', PageTitle = "Matplotlib", quartiere=quartiereUtente, tabella = fontQuart.to_html())
+
+@app.route("/fontanelle.png", methods=["GET"])
+def Fontanelle():
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    imgUtente.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    fontQuart.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    contextily.add_basemap(ax=ax)   
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
