c948361285863ca865897f12084f0705ae6d755b
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2022-04-06 21:18:47 +0000

    finito

--- Esercizi/CorrezioneVerificaC/crb.py
+++ Esercizi/CorrezioneVerificaC/crb.py
@@ -26,7 +26,7 @@ def home():
 def selezione():
     scelta = request.args["scelta"]
     if scelta=="es1":
-        return redirect(url_for("numero")) #tra gli apici la directory
+        return redirect(url_for("lunghezzaKm")) #tra gli apici la directory
     elif scelta=="es2":
         return redirect(url_for("sceltaqt"))
     elif scelta=="es3":
@@ -34,6 +34,38 @@ def selezione():
 
 #1. Avere l’elenco delle linee tranviarie e di bus che hanno un percorso la cui lunghezza è compresa tra due valori
 #inseriti dall’utente. Ordinare le linee in ordine crescente sul numero della linea.
+
+@app.route('/lunghezzaKm', methods=['GET'])
+def lunghezzaKm():
+    return render_template('lunghezzaKm.html')
+
+@app.route('/MappaFinale', methods=['GET'])
+def MappaFinale():
+    global Min,Max,linee_distanza
+   
+    Min = min(request.args["Kmmin"], request.args["Kmmax"])#ritorna il numero piu piccolo tra i 2 valori
+    Max = max(request.args["Kmmin"], request.args["Kmmax"])#ritorna il numero piu grande tra i 2 valori
+
+    linee_distanza = Mezzi[(Mezzi["lung_km"] > Min) & (Mezzi["lung_km"] < Max)].sort_values("linea")
+    return render_template("MappaFinale.html", tabella = linee_distanza.to_html())
+
+
+@app.route("/mappa2.png", methods=["GET"])
+
+def mappa2png():
+    
+
+    fig, ax = plt.subplots(figsize = (12,8))
+
+
+    linee_distanza.to_crs(epsg=3857).plot(ax=ax)
+    contextily.add_basemap(ax=ax)   
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
 
 
 #2.Avere un elenco di tutte le linee (tram e bus) che passano in un certo quartiere. L’utente inserisce il nome del
@@ -50,8 +82,8 @@ def visualizzamezzi():
     nome_quartiere=request.args["quartiere"]
     quartiere=quartieri[quartieri.NIL.str.contains(nome_quartiere)]
     mezzi_quartiere=Mezzi[Mezzi.intersects(quartiere.geometry.squeeze())]
-    mezzi_quartiere=mezzi_quartiere.astype({"linea":int})
-    print(mezzi_quartiere)
+    mezzi_quartiere=mezzi_quartiere.astype({"linea":int})#solo la colonna divnta un intero 
+    
     return render_template('visualizzamezzi.html',risultato=sorted(list(mezzi_quartiere.linea.drop_duplicates()))) #sorted riordina la lista
 
 #3. Avere la mappa della città con il percorso di una linea scelta dall’utente. L’utente sceglie il numero della linea da
@@ -85,12 +117,6 @@ def mappapng():
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
-
-
-
-
-
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
